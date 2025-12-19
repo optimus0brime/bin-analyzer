6e164a029ae3c9465e4454b3795f9b6663c1e605
optimus0brime <optimus0brime@gmail.com>
2025-12-20 00:41:01 +0530

    updated ignore and yara...

--- Makefile
+++ Makefile
@@ -1,12 +1,28 @@
 CC = gcc
-CFLAGS = -Wall -Wextra -std=c11 -O2
-OBJS = main.o detect.o analyze.o view.o utils.o
+CFLAGS = -Wall -Wextra -std=c11 -O2 -Iinclude
+SRCDIR = src
+BUILDDIR = build
+SOURCES = $(wildcard $(SRCDIR)/*.c)
+OBJS = $(SOURCES:$(SRCDIR)/%.c=$(BUILDDIR)/%.o)
 
+# Default target
+all: binanalyzer
+
+# Create build directory
+$(BUILDDIR):
+	mkdir -p $(BUILDDIR)
+
+# Compile sources to objects
+$(BUILDDIR)/%.o: $(SRCDIR)/%.c | $(BUILDDIR)
+	$(CC) $(CFLAGS) -c $< -o $@
+
+# Link final executable
 binanalyzer: $(OBJS)
 	$(CC) $(CFLAGS) -o binanalyzer $(OBJS) -lm
 
+# Clean build artifacts
 clean:
-	rm -f *.o binanalyzer
+	rm -rf $(BUILDDIR) binanalyzer
 
-.PHONY: clean
+.PHONY: all clean
 
--- src/yara_lite.c
+++ src/yara_lite.c
@@ -1,21 +1,29 @@
-// yara_lite.c
+#define _GNU_SOURCE   // ← This enables memmem()
+
 #include <stdio.h>
 #include <string.h>
+#include "yara_lite.h"  // if you have a header
 
 void scan_with_embedded_yara(const unsigned char *data, size_t size) {
     printf("[+] Embedded YARA-like rule scan (malware IOCs):\n");
 
     const char *rules[] = {
-        "UPX!", "This program cannot be run in DOS mode", // Common in PE
+        "UPX!", "This program cannot be run in DOS mode",
         "VirtualAlloc", "CreateRemoteThread", "WriteProcessMemory",
         "IsDebuggerPresent", "anti-debug", "sandbox",
         "http://", "https://", "cmd.exe", "powershell.exe",
+        "MZ", "PE\0\0",  // Raw bytes for PE signature
         NULL
     };
 
     for (int i = 0; rules[i]; i++) {
-        if (memmem(data, size, rules[i], strlen(rules[i]))) {
-            printf("   !!!  Match: \"%s\"\n", rules[i]);
+        const char *rule = rules[i];
+        size_t rule_len = strlen(rule);
+
+        if (memmem(data, size, rule, rule_len)) {
+            printf("   !!!  Match: \"%s\"\n", rule);
         }
     }
+
+    printf("[+] YARA-lite scan complete\n");
 }
